3c364635db0a5b3e5e9cec05dbdfc7d32964455f
Arun Chouhan <108116902+arunchouhan07@users.noreply.github.com>
2022-07-13 22:56:23 +0530

    Search 2D row and column wise sorted matrix

---
+++ DSA/Search_2D_Sorted_Matrix.java
@@ -0,0 +1,35 @@
+/*************************************
+ 
+    Following is the Pair Class structure.
+    
+    class Pair{
+        int x; int y;
+        
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+*************************************/
+
+public class Solution {
+	public static Pair search(int [][] matrix, int x) {
+		Pair p=new Pair(-1,-1);
+        int row=0;
+        int colume=matrix[row].length-1;
+        while(row<matrix.length && colume>=0){
+            if(matrix[row][colume]==x){
+                Pair p1=new Pair(row,colume);
+                return p1;
+            }
+            else if(matrix[row][colume]>x){
+                colume--;
+            }
+            else {
+                row++;
+            }
+        }
+        return p;
+	}
+}
